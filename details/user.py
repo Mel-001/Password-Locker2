2f1b6c844c8279d5c5834b03bc25d23244fce92e
Meloni <meloni.langat@student.moringaschool.com>
2022-03-11 12:59:47 +0300

    create empty user list

--- details/user.py
+++ details/user.py
@@ -2,3 +2,16 @@ class User:
     '''
     Class to create user instances 
     '''
+    users_list = []
+
+    def __init__ (self, user_name, password):
+
+        '''
+        properties of user 
+
+        args:
+        user_name
+        password
+        '''
+        self.user_name = user_name
+        self.password = password
