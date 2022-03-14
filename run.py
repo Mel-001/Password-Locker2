13dabffbc3cee9b831529be40d8d4328c8e9a128
Meloni <meloni.langat@student.moringaschool.com>
2022-03-14 08:57:22 +0300

    show  user

--- run.py
+++ run.py
@@ -11,3 +11,8 @@ def create_new_user(user_name, password):
     return new_user
 def save_new_user(user):
     user.save_this_user
+def show_user(user):
+    '''
+    this function displays a user
+    '''
+    user.show_user()
