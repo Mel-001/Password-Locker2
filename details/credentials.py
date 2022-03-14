94c30f200034c31cb2b881d698c731514880a057
Meloni <meloni.langat@student.moringaschool.com>
2022-03-14 08:49:13 +0300

    correct indentation

--- details/credentials.py
+++ details/credentials.py
@@ -21,8 +21,22 @@ class Credentials:
         self.user_account = user_account
         self.user_name = user_name
         self.password = password
-        
-        def save_credentials(self):
+
+
+    # @classmethod
+    # def validate_user(cls, user_name, password):
+    #     '''
+    #     check whether a user exists
+    #     '''
+    #     validate_user = ''
+    #     for user in User.users_list:
+    #         if user.user_name == user_name and user.password == password:
+    #             validate_user = user.user_name
+    #             return validate_user
+    
+    
+    
+    def save_credentials(self):
 
         '''
         add new credential to credentials_list
@@ -31,17 +45,19 @@ class Credentials:
         Credentials.credentials_list.append(self)
 
     @classmethod 
+
     def search_user_credentials(cls, user_account):
 
         '''
-        method that searches user credentials
+        this method searches users credentials
         '''
         
         for credential in cls.credentials_list:
             if credential.user_account == user_account:
                 return credential
-            @classmethod
-            def show_credentials(cls):
+    
+    @classmethod
+    def show_credentials(cls):
 
         '''
         function displays user credentials
@@ -49,6 +65,7 @@ class Credentials:
 
 
         return cls.credentials_list
+    
     @classmethod
     def delete_credentials_account(cls, user_account):
         '''
@@ -59,7 +76,9 @@ class Credentials:
             if credentials_list.user_account == user_account:
                 cls.credentials_list.remove(credentials_list)
                 return cls.credentials_list
-            def generate_password(length=6):
+    
+    
+    def generate_password(length=6):
         '''
         generate random password
         '''
